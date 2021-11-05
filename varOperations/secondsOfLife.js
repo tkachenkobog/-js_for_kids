953c416cfa6044e69ffb558c0073d6407b93793f
tkachenkobog <93750185+tkachenkobog@users.noreply.github.com>
2021-11-05 05:38:13 +0300

    Create secondsOfLife.js

---
+++ varOperations/secondsOfLife.js
@@ -0,0 +1,7 @@
+var SecondInMinutes=60;
+var MinutesInHour=60;
+var SecondInHour=MinutesInHour*SecondInMinutes;
+var SecondInDay=24*SecondInHour;
+var SecondInYear=365*SecondInDay;
+var SecondOfLife=31*SecondInYear;
+console.log(SecondOfLife);
